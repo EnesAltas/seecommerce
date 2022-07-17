6c1a0d824cd200ebb745fb3f462e1a326feecb59
Enes <enes_ltas@hotmail.com>
2022-07-17 15:58:00 +0300

    Update orders.js

--- public/script/orders.js
+++ public/script/orders.js
@@ -73,9 +73,9 @@ function searchBox(product){
   .then((result) => {
       if(result.Data != 'Not Found'){
         var data = JSON.parse(result.Data)
+	$('#result_output').empty();
         data.forEach(product => {
           resultbox.style.display = 'block'
-          $('#result_output').empty();
           var html = `<a style="color:black; " href="/product/${product.url}"><li style="padding: 5px;">${product.producthead}<li></a>`
           $('#result_output').append(html);
         });
